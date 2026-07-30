ce7213af48bb318ffd9ce87de5c97f2d3ba95314
René Viscarra <reneviscarra42@gmail.com>
2026-07-30 11:52:28 -0600

    act

--- JS/registerING.js
+++ JS/registerING.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("📋 Usuarios registrados:", usuarios);
     alert(`¡Registro exitoso! Bienvenido, ${name} 🎉`);
 
-    window.location.href = "../INGLES/login.html"; 
+    window.location.href = "../../HTML/INGLES/loginING.html"; 
   });
 
   function validarNombre(name) {
